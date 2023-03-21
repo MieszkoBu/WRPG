e8d6cc7e91ff454a2ee128492bdcfa9c87e3730b
MieszkoBu <115088598+MieszkoBu@users.noreply.github.com>
2023-03-21 16:12:42 +0100

    Update Zadanie3.php

--- Zadanie3.php
+++ Zadanie3.php
@@ -52,4 +52,51 @@ echo "Największy element tablicy to " . max_while($tablica) . "<br>";
 echo "Największy element tablicy to " . max_do_while($tablica) . "<br>";
 echo "Największy element tablicy to " . max_foreach($tablica) . "<br>";
 
+//Zadanie 3.2
+
+function rzuty_koscia($rzuty) {
+  $wyniki = array();
+  for ($i = 0; $i < $rzuty; $i++) {
+    $wynik = rand(1, 6);
+    array_push($wyniki, $wynik);
+  }
+  return $wyniki;
+}
+$wyniki = rzuty_koscia(7);
+print_r($wyniki);
+
+//Zadanie 3.3
+
+function tabliczka_mnozenia($rozmiar) {
+    for ($i = 1; $i <= $rozmiar; $i++) {
+      for ($j = 1; $j <= $rozmiar; $j++) {
+        echo str_pad($i * $j, 4, " ", STR_PAD_LEFT);
+      }
+      echo "<br>";
+    }
+  }
+tabliczka_mnozenia(12);
+
+//Zadanie 3.4
+
+function czy_pierwsza($liczba) {
+    $iteracja = 0;
+  
+    if ($liczba < 2) {
+      return false;
+    }
+  
+    for ($i = 2; $i <= sqrt($liczba); $i++) {
+      $iteracja++;
+      if ($liczba % $i == 0) {
+        return false;
+      }
+    }
+  
+    echo "Liczba iteracji: $iteracja" . "<br>";
+    return true;
+  }
+  $liczba = 8;
+  echo czy_pierwsza($liczba) ? "Liczba $liczba jest liczbą pierwszą.\n" : "Liczba $liczba nie jest liczbą pierwszą.\n";
+
 ?>
