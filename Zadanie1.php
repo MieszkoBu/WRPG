9f6e2c8111352cfd340434ea6ad5eb6b28c3601a
MieszkoBu <115088598+MieszkoBu@users.noreply.github.com>
2023-03-21 07:23:53 +0100

    Update Zadanie1.php

--- Zadanie1.php
+++ Zadanie1.php
@@ -39,5 +39,51 @@ function data($pesel){
 $pesel=123456789;
 echo data($pesel) . "<br>";
 // Zadanie 1.5
+function oblicz_pole_trojkata($podstawa, $wysokosc) {
+    return 0.5 * $podstawa * $wysokosc;
+}
 
+function oblicz_pole_prostokata($dlugosc, $szerokosc) {
+    return $dlugosc * $szerokosc;
+}
+
+function oblicz_pole_trapezu($podstawa1, $podstawa2, $wysokosc) {
+    return 0.5 * ($podstawa1 + $podstawa2) * $wysokosc;
+}
+echo "Wybierz figurę, dla której chcesz obliczyć pole:" . "<br>";
+echo "1. Trójkąt" . "<br>";
+echo "2. Prostokąt" . "<br>";
+echo "3. Trapez" . "<br>";
+$wybor = "2";
+switch ($wybor) {
+    case 1:
+        echo "Podaj długość podstawy trójkąta: " . "<br>";
+        $podstawa = 4;
+        echo "Podaj wysokość trójkąta: " . "<br>";
+        $wysokosc = 3;
+        $pole = oblicz_pole_trojkata($podstawa, $wysokosc);
+        echo "Pole trójkąta wynosi: " . $pole;
+        break;
+    case 2:
+        echo "Podaj długość boku a prostokąta: " . "<br>";
+        $a = 6;
+        echo "Podaj długość boku b prostokąta: " . "<br>";
+        $b = 5;
+        $pole = oblicz_pole_prostokata($a, $b);
+        echo "Pole prostokąta wynosi: " . $pole;
+        break;
+    case 3:
+        echo "Podaj długość pierwszej podstawy trapezu: " . "<br>";
+        $podstawa1 = 3;
+        echo "Podaj długość drugiej podstawy trapezu: " . "<br>";
+        $podstawa2 = 4;
+        echo "Podaj wysokość trapezu: ";
+        $wysokosc = 6;
+        $pole = oblicz_pole_trapezu($podstawa1, $podstawa2, $wysokosc);
+        echo "Pole trapezu wynosi: " . $pole;
+        break;
+    default:
+        echo "Nieprawidłowy wybór!";
+        break;
+}
 ?>
